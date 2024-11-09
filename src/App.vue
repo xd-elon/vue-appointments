664f31da33f9b5a8f588118783edbb05b3c887ee
xd-elon <alandevelope@gmail.com>
2024-11-09 17:01:26 -0300

    appointsments vue

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+  
+</script>
+
+<template>
+  <header-principal />
+  <main>
+    <div>
+      <span>Hello world</span>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+
+
+</style>
---
+++ src/components/header-principal.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header>
+    <!-- <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" /> -->
+     
+  </header>
+</template>
+
+<style scoped>
+
+
+</style>
